aa8211b20bcd620f4240941a66dc2b6672e29a28
Florian Lüdiger <florian.luediger@adesso.de>
2022-04-10 14:04:05 +0200

    Implement collection of form state

--- src/ContentBase.tsx
+++ src/ContentBase.tsx
@@ -1,9 +1,12 @@
 import { PlaylistList } from "./PlaylistList/PlaylistList";
 import { useAuth } from "react-oauth2-pkce";
-import React from "react";
+import React, { FormEvent, useState } from "react";
 
 export function ContentBase() {
   const { authService } = useAuth();
+  const [includePlaylists, setIncludePlaylists] = useState<string[]>([]);
+  const [excludePlaylists, setExcludePlaylists] = useState<string[]>([]);
+  const [newName, setNewName] = useState("");
 
   if (!authService.isAuthenticated()) {
     return (
@@ -14,28 +17,49 @@ export function ContentBase() {
       </div>
     );
   }
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    console.log("IncludePlaylists:");
+    console.log(includePlaylists);
+    console.log("ExcludePlaylists");
+    console.log(excludePlaylists);
+    console.log("Name");
+    console.log(newName);
+  }
+
   return (
     <div className="flex justify-center pt-10">
-      <div className="grid grid-cols-1 md:grid-cols2">
+      <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols2">
         <div className="mx-5">
           <p className="text-xl">I want all songs from...</p>
-          <PlaylistList />
+          <PlaylistList
+            checkedPlaylists={includePlaylists}
+            setCheckedList={setIncludePlaylists}
+          />
         </div>
         <div className="mx-5">
           <p className="text-xl mt-5 md:mt-0">... but no songs from...</p>
-          <PlaylistList />
+          <PlaylistList
+            checkedPlaylists={excludePlaylists}
+            setCheckedList={setExcludePlaylists}
+          />
         </div>
         <div className="col-span-1 md:col-span-2 mt-5">
           <p className="text-xl ml-5 inline">... in a playlist called</p>
           <input
             className="border-b-2 border-slate-400 ml-3 focus:outline-none focus:border-black text-xl inline"
             type="text"
+            value={newName}
+            onChange={(name) => setNewName(name.target.value)}
           />
-          <button className="text-xl ml-5 inline border-2 border-slate-500 rounded-lg px-3 py-2 mt-2 md:mt-0 hover:bg-slate-100">
-            Let's go!
-          </button>
+          <input
+            type="submit"
+            value="Let's go!"
+            className="text-xl ml-5 inline border-2 border-slate-500 rounded-lg px-3 py-2 mt-2 md:mt-0 hover:bg-slate-100"
+          />
         </div>
-      </div>
+      </form>
     </div>
   );
 }
--- src/PlaylistList/PlaylistList.tsx
+++ src/PlaylistList/PlaylistList.tsx
@@ -2,7 +2,13 @@ import { useAuth } from "react-oauth2-pkce";
 import { usePlaylists } from "./usePlaylists";
 import { PlaylistListItem } from "./PlaylistListItem";
 
-export function PlaylistList() {
+export function PlaylistList({
+  checkedPlaylists,
+  setCheckedList,
+}: {
+  checkedPlaylists: string[];
+  setCheckedList: (list: string[]) => void;
+}) {
   const { authService } = useAuth();
   const { playlists, error } = usePlaylists(
     authService.getAuthTokens().access_token
@@ -11,12 +17,21 @@ export function PlaylistList() {
   if (error) return <div>Failed to load</div>;
   if (!playlists) return <div>Loading...</div>;
 
+  function onItemChange(playlistId: string, checked: boolean) {
+    if (checked) {
+      checkedPlaylists.push(playlistId);
+      setCheckedList(checkedPlaylists);
+    } else {
+      setCheckedList(checkedPlaylists.filter((p) => p !== playlistId));
+    }
+  }
+
   return (
     <div>
       <div className="rounded overflow-hidden shadow-lg">
         <div className="m-5">
           {playlists.map((p) => (
-            <PlaylistListItem playlist={p} />
+            <PlaylistListItem playlist={p} onChange={onItemChange} />
           ))}
         </div>
       </div>
